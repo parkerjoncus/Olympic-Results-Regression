834a4a77ca5099775bb0582a3d9a83e2a537cc43
edebroux <39842613+edebroux@users.noreply.github.com>
2018-11-30 15:03:39 -0600

    Update MATH 484 PROJECT.py

--- olympic-track-field-results/MATH 484 PROJECT.py
+++ olympic-track-field-results/MATH 484 PROJECT.py
@@ -72,7 +72,7 @@ model_100M_Women.summary()
 
 # 200 M Men's and Women's Events now:
 
-# In[8]:
+# In[7]:
 
 
 X = tf_results['200M Men'].Year.astype(float)
@@ -83,7 +83,7 @@ print(model_200M_Men.params)
 model_200M_Men.summary()
 
 
-# In[9]:
+# In[8]:
 
 
 X = tf_results['200M Women'].Year.astype(float)
@@ -94,7 +94,7 @@ print(model_200M_Women.params)
 model_200M_Women.summary()
 
 
-# In[11]:
+# In[9]:
 
 
 X = tf_results['Long Jump Men'].Year.astype(float)
@@ -105,7 +105,7 @@ print(LJ_Men.params)
 LJ_Men.summary()
 
 
-# In[12]:
+# In[10]:
 
 
 X = tf_results['Long Jump Women'].Year.astype(float)
@@ -116,7 +116,7 @@ print(LJ_Women.params)
 LJ_Women.summary()
 
 
-# In[14]:
+# In[11]:
 
 
 X = tf_results['Shot Put Men'].Year.astype(float)
@@ -127,7 +127,7 @@ print(SP_Men.params)
 SP_Men.summary()
 
 
-# In[15]:
+# In[12]:
 
 
 X = tf_results['Shot Put Women'].Year.astype(float)
@@ -138,7 +138,7 @@ print(SP_Women.params)
 SP_Women.summary()
 
 
-# In[17]:
+# In[13]:
 
 
 #Men's 1500M
@@ -151,7 +151,7 @@ print(Men_1500M.params)
 Men_1500M.summary()
 
 
-# In[18]:
+# In[14]:
 
 
 #Women's 1500M
@@ -165,7 +165,7 @@ print(Women_1500M.params)
 Women_1500M.summary()
 
 
-# In[132]:
+# In[15]:
 
 
 #Regression Subplots
@@ -252,7 +252,7 @@ plt.show()
 # 
 # We now will look at the residuals to try to determine if we can make the assumption that the error is normally distributed with mean 0 and variance $\sigma^2$, and run some diagnostic tests to determine whether the error is constant.
 
-# In[91]:
+# In[16]:
 
 
 #Residuals for 100M
@@ -276,7 +276,7 @@ res_1500_m = men_1500m['seconds'] - (846.904980 -0.316465 * men_1500m['Year'])
 res_1500_w = women_1500m['seconds'] - (40.274876 +0.101555 * women_1500m['Year'])
 
 
-# In[131]:
+# In[17]:
 
 
 #Residual Plots for Simple Linear Models
@@ -290,12 +290,12 @@ plt.title('Residuals for 100M')
 plt.legend(['100M Men Residuals', '100M Women Residuals'])
 
 plt.subplot(322)
-plt.scatter(tf_results['100M Men'].Year, res_100_m, color = 'k')
-plt.scatter(tf_results['100M Women'].Year, res_100_w, color = 'm')
+plt.scatter(tf_results['200M Men'].Year, res_200_m, color = 'k')
+plt.scatter(tf_results['200M Women'].Year, res_200_w, color = 'm')
 plt.xlabel('Year')
 plt.ylabel('Residual')
-plt.title('Residuals for 100M')
-plt.legend(['100M Men Residuals', '100M Women Residuals'])
+plt.title('Residuals for 200M')
+plt.legend(['200M Men Residuals', '200M Women Residuals'])
 
 plt.subplot(323)
 plt.scatter(men_1500m['Year'], res_1500_m, color = 'c')
@@ -330,7 +330,7 @@ plt.show()
 # 
 # # Normal Probability Plots
 
-# In[130]:
+# In[18]:
 
 
 #Normal Probability Plots
@@ -407,7 +407,7 @@ plt.show()
 # 
 # Critical value for the rejection region would be $\chi^2_{\alpha/2; 1}$, and we will test at an $\alpha$ = 0.05 level.
 
-# In[107]:
+# In[19]:
 
 
 from statsmodels.stats import diagnostic as dn
@@ -415,63 +415,63 @@ bp_100_m = dn.het_breuschpagan(model_100M_Men.resid, model_100M_Men.model.exog)
 print("The Breusch-Pagan test yields a p-value of: ", bp_100_m[3],".")
 
 
-# In[108]:
+# In[20]:
 
 
 bp_100_w = dn.het_breuschpagan(model_100M_Women.resid, model_100M_Women.model.exog)
 print("The Breusch-Pagan test yields a p-value of: ", bp_100_w[3],".")
 
 
-# In[109]:
+# In[21]:
 
 
 bp_200_m = dn.het_breuschpagan(model_200M_Men.resid, model_200M_Men.model.exog)
 print("The Breusch-Pagan test yields a p-value of: ", bp_200_m[3],".")
 
 
-# In[110]:
+# In[22]:
 
 
 bp_200_w = dn.het_breuschpagan(model_200M_Women.resid, model_200M_Women.model.exog)
 print("The Breusch-Pagan test yields a p-value of: ", bp_200_w[3],".")
 
 
-# In[111]:
+# In[23]:
 
 
 bp_1500_m = dn.het_breuschpagan(Men_1500M.resid, Men_1500M.model.exog)
 print("The Breusch-Pagan test yields a p-value of: ", bp_1500_m[3],".")
 
 
-# In[112]:
+# In[24]:
 
 
 bp_1500_w = dn.het_breuschpagan(Women_1500M.resid, Women_1500M.model.exog)
 print("The Breusch-Pagan test yields a p-value of: ", bp_1500_w[3],".")
 
 
-# In[113]:
+# In[25]:
 
 
 bp_lj_m = dn.het_breuschpagan(LJ_Men.resid, LJ_Men.model.exog)
 print("The Breusch-Pagan test yields a p-value of: ", bp_lj_m[3],".")
 
 
-# In[114]:
+# In[26]:
 
 
 bp_lj_w = dn.het_breuschpagan(LJ_Women.resid, LJ_Women.model.exog)
 print("The Breusch-Pagan test yields a p-value of: ", bp_lj_w[3],".")
 
 
-# In[115]:
+# In[27]:
 
 
 bp_sp_m = dn.het_breuschpagan(SP_Men.resid, SP_Men.model.exog)
 print("The Breusch-Pagan test yields a p-value of: ", bp_sp_m[3],".")
 
 
-# In[116]:
+# In[28]:
 
 
 bp_sp_w = dn.het_breuschpagan(SP_Women.resid, SP_Women.model.exog)
@@ -501,3 +501,456 @@ print("The Breusch-Pagan test yields a p-value of: ", bp_sp_w[3],".")
 # # ALL THE CODE WILL BE IN R FOR THESE TESTS.
 # 
 # All of the results are on Github. In all cases we conclude that the simple linear models are not appropriate. Clearly, we should use some Box-Cox transformations on our results in order to better predict the results.
+# 
+# # Multiple Linear Regression - Standardized
+
+# In[29]:
+
+
+from scipy.stats.mstats import zscore
+Mens_100M = pd.read_csv('X100M_Men.csv')
+Mens_100M.head()
+
+
+# In[30]:
+
+
+X = Mens_100M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Mens_100M['Result'].astype(float)
+#X = sm.add_constant(X)
+big_100M_Men = sm.OLS(zscore(y), zscore(X)).fit()
+print(big_100M_Men.summary())
+
+
+# In[31]:
+
+
+Womens_100M = pd.read_csv('100M_Women.csv')
+Womens_100M.head()
+
+
+# In[32]:
+
+
+X = Womens_100M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Womens_100M['Result'].astype(float)
+#X = sm.add_constant(X)
+big_100M_Women = sm.OLS(zscore(y), zscore(X)).fit()
+print(big_100M_Women.params)
+print(big_100M_Women.summary())
+
+
+# In[33]:
+
+
+Mens_200M = pd.read_csv('200M_Men.csv')
+X = Mens_200M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Mens_200M['Result'].astype(float)
+#X = sm.add_constant(X)
+big_200M_Men = sm.OLS(zscore(y), zscore(X)).fit()
+print(big_200M_Men.params)
+print(big_200M_Men.summary())
+
+
+# In[34]:
+
+
+Womens_200M = pd.read_csv('200M_Women.csv')
+X = Womens_200M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Womens_200M['Result'].astype(float)
+#X = sm.add_constant(X)
+big_200M_Women = sm.OLS(zscore(y), zscore(X)).fit()
+print(big_200M_Women.params)
+print(big_200M_Women.summary())
+
+
+# In[35]:
+
+
+Mens_1500M = pd.read_csv('1500M_Men.csv')
+X = Mens_1500M[['Year', 'age', 'weight', 'height']].astype(float)
+y = Mens_1500M['seconds'].astype(float)
+#X = sm.add_constant(X)
+big_1500M_Men = sm.OLS(zscore(y), zscore(X)).fit()
+print(big_1500M_Men.summary())
+
+
+# In[36]:
+
+
+Womens_1500M = pd.read_csv('1500M_Women.csv')
+X = Womens_1500M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Womens_1500M['seconds'].astype(float)
+#X = sm.add_constant(X)
+big_1500M_Women = sm.OLS(zscore(y), zscore(X)).fit()
+print(big_1500M_Women.params)
+print(big_1500M_Women.summary())
+
+
+# In[37]:
+
+
+Mens_LJ = pd.read_csv('LJ_Men.csv')
+X = Mens_LJ[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Mens_LJ['Result'].astype(float)
+#X = sm.add_constant(X)
+big_Mens_LJ= sm.OLS(zscore(y), zscore(X)).fit()
+print(big_Mens_LJ.params)
+print(big_Mens_LJ.summary())
+
+
+# In[38]:
+
+
+Mens_SP = pd.read_csv('SP_Men.csv')
+X = Mens_SP[['Age', 'Weight', 'Height']].astype(float)
+y = Mens_SP['Result'].astype(float)
+#X = sm.add_constant(X)
+big_Mens_SP= sm.OLS(zscore(y), zscore(X)).fit()
+print(big_Mens_SP.params)
+print(big_Mens_SP.summary())
+
+
+# In[39]:
+
+
+res_prob_sp_w = stats.probplot(big_Mens_SP.resid, plot= plt)
+plt.title('Mens Shot Put Probability Plot')
+plt.show()
+
+
+# In[40]:
+
+
+Mens_SP = pd.read_csv('SP_Men.csv')
+X = Mens_SP[['Year','Age', 'Weight', 'Height']].astype(float)
+y = Mens_SP['Result'].astype(float)
+#X = sm.add_constant(X)
+big_Mens_SP= sm.OLS(zscore(y), zscore(X)).fit()
+print(big_Mens_SP.params)
+print(big_Mens_SP.summary())
+
+
+# In[41]:
+
+
+Womens_LJ = pd.read_csv('LJ_Women.csv')
+X = Womens_LJ[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Womens_LJ['Result'].astype(float)
+#X = sm.add_constant(X)
+big_Womens_LJ= sm.OLS(zscore(y), zscore(X)).fit()
+print(big_Womens_LJ.summary())
+
+
+# In[42]:
+
+
+Womens_SP = pd.read_csv('SP_Women.csv')
+X = Womens_SP[['Year','Age', 'Weight', 'Height']].astype(float)
+y = Womens_SP['Result'].astype(float)
+#X = sm.add_constant(X)
+big_Womens_SP= sm.OLS(zscore(y), zscore(X)).fit()
+print(big_Womens_SP.params)
+print(big_Womens_SP.summary())
+
+
+# In[43]:
+
+
+#Standardized Normal Probability Plots for Multiple Linear Regression Models
+plt.figure(figsize=(20, 10))
+plt.subplot(251)
+#100M Men
+res_prob_100m_m = stats.probplot(big_100M_Men.resid, plot= plt)
+plt.title('100M Men Probability Plot')
+
+plt.subplot(252)
+#100M Women
+res_prob_100m_w = stats.probplot(big_100M_Women.resid, plot= plt)
+plt.title('100M Women Probability Plot')
+
+plt.subplot(253)
+#200M Men
+res_prob_200m_m = stats.probplot(big_200M_Men.resid, plot= plt)
+plt.title('200M Men Probability Plot')
+
+plt.subplot(254)
+#200M Women
+res_prob_200m_w = stats.probplot(big_200M_Women.resid, plot= plt)
+plt.title('200M Women Probability Plot')
+
+plt.subplot(255)
+#1500M Men
+res_prob_1500m_m = stats.probplot(big_1500M_Men.resid, plot= plt)
+plt.title('1500M Men Probability Plot')
+
+plt.subplot(256)
+#1500M Women
+res_prob_1500m_w = stats.probplot(big_1500M_Women.resid, plot= plt)
+plt.title('1500M Women Probability Plot')
+
+plt.subplot(257)
+#Men's Shot Put
+res_prob_sp_m = stats.probplot(big_Mens_SP.resid, plot= plt)
+plt.title('Shot Put Men Probability Plot')
+
+plt.subplot(258)
+#Women's Shot Put
+res_prob_sp_w = stats.probplot(big_Womens_SP.resid, plot= plt)
+plt.title('Shot Put Women Probability Plot')
+
+plt.subplot(259)
+#Men's Long Jump
+res_prob_lj_m = stats.probplot(big_Mens_LJ.resid, plot= plt)
+plt.title('Long Jump Men Probability Plot')
+
+plt.subplot(2,5,10)
+#Women's Long Jump
+res_prob_lj_w = stats.probplot(big_Womens_LJ.resid, plot= plt)
+plt.title('Long Jump Women Probability Plot')
+
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.25, hspace=0.25)
+plt.savefig('std_mlr_normprob_results.png')
+plt.show()
+
+
+# In[44]:
+
+
+#Standardized Residual Plots for Simple Linear Models
+plt.figure(figsize = (16, 12))
+plt.subplot(321)
+plt.scatter(Mens_100M['Year'], big_100M_Men.resid, color = 'k')
+plt.scatter(Womens_100M['Year'], big_100M_Women.resid, color = 'm')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for 100M')
+plt.legend(['100M Men Residuals', '100M Women Residuals'])
+
+plt.subplot(322)
+plt.scatter(Mens_200M.Year, big_200M_Men.resid, color = 'k')
+plt.scatter(Womens_200M.Year, big_200M_Women.resid, color = 'm')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for 200M')
+plt.legend(['200M Men Residuals', '200M Women Residuals'])
+
+plt.subplot(323)
+plt.scatter(Mens_1500M.Year, big_1500M_Men.resid, color = 'c')
+plt.scatter(Womens_1500M.Year, big_1500M_Women.resid, color = 'y')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for 1500M')
+plt.legend(['1500M Men Residuals', '1500M Women Residuals'])
+
+plt.subplot(324)
+plt.scatter(Mens_SP.Year, big_Mens_SP.resid, color = 'b')
+plt.scatter(Womens_SP.Year, big_Womens_SP.resid, color = 'g')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for Shot Put')
+plt.legend(['Shot Put Men Residuals', 'Shot Put Women Residuals'])
+
+plt.subplot(325)
+plt.scatter(Mens_LJ.Year, big_Mens_LJ.resid, color = 'b')
+plt.scatter(Womens_LJ.Year, big_Womens_LJ.resid, color = 'g')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for Long Jump')
+plt.legend(['Long Jump Men Residuals', 'Long Jump Women Residuals'])
+
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.25)
+plt.savefig('std_mlr_residual_results.png')
+plt.show()
+
+
+# # Multiple Linear Regression - Unstandardized
+
+# In[45]:
+
+
+#Unstandardized Full First Order Models
+X = Mens_100M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Mens_100M['Result'].astype(float)
+X = sm.add_constant(X)
+big_100M_Men = sm.OLS(y, X).fit()
+print(big_100M_Men.summary())
+
+X = Womens_100M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Womens_100M['Result'].astype(float)
+X = sm.add_constant(X)
+big_100M_Women = sm.OLS(y, X).fit()
+print(big_100M_Women.summary())
+
+Mens_200M = pd.read_csv('200M_Men.csv')
+X = Mens_200M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Mens_200M['Result'].astype(float)
+X = sm.add_constant(X)
+big_200M_Men = sm.OLS(y, X).fit()
+print(big_200M_Men.summary())
+
+Womens_200M = pd.read_csv('200M_Women.csv')
+X = Womens_200M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Womens_200M['Result'].astype(float)
+X = sm.add_constant(X)
+big_200M_Women = sm.OLS(y, X).fit()
+print(big_200M_Women.summary())
+
+Mens_1500M = pd.read_csv('1500M_Men.csv')
+X = Mens_1500M[['Year', 'age', 'weight', 'height']].astype(float)
+y = Mens_1500M['seconds'].astype(float)
+X = sm.add_constant(X)
+big_1500M_Men = sm.OLS(y, X).fit()
+print(big_1500M_Men.summary())
+
+Womens_1500M = pd.read_csv('1500M_Women.csv')
+X = Womens_1500M[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Womens_1500M['seconds'].astype(float)
+X = sm.add_constant(X)
+big_1500M_Women = sm.OLS(y, X).fit()
+print(big_1500M_Women.summary())
+
+Mens_LJ = pd.read_csv('LJ_Men.csv')
+X = Mens_LJ[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Mens_LJ['Result'].astype(float)
+X = sm.add_constant(X)
+big_Mens_LJ= sm.OLS(y, X).fit()
+print(big_Mens_LJ.summary())
+
+Mens_SP = pd.read_csv('SP_Men.csv')
+X = Mens_SP[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Mens_SP['Result'].astype(float)
+X = sm.add_constant(X)
+big_Mens_SP= sm.OLS(y, X).fit()
+print(big_Mens_SP.summary())
+
+Womens_LJ = pd.read_csv('LJ_Women.csv')
+X = Womens_LJ[['Year', 'Age', 'Weight', 'Height']].astype(float)
+y = Womens_LJ['Result'].astype(float)
+X = sm.add_constant(X)
+big_Womens_LJ= sm.OLS(y, X).fit()
+print(big_Womens_LJ.summary())
+
+Womens_SP = pd.read_csv('SP_Women.csv')
+X = Womens_SP[['Year','Age', 'Weight', 'Height']].astype(float)
+y = Womens_SP['Result'].astype(float)
+#X = sm.add_constant(X)
+big_Womens_SP= sm.OLS(y, X).fit()
+print(big_Womens_SP.summary())
+
+
+# In[46]:
+
+
+#Unstandardized Residual Plots for Simple Linear Models
+plt.figure(figsize = (16, 12))
+plt.subplot(321)
+plt.scatter(Mens_100M['Year'], big_100M_Men.resid, color = 'k')
+plt.scatter(Womens_100M['Year'], big_100M_Women.resid, color = 'm')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for 100M')
+plt.legend(['100M Men Residuals', '100M Women Residuals'])
+
+plt.subplot(322)
+plt.scatter(Mens_200M.Year, big_200M_Men.resid, color = 'k')
+plt.scatter(Womens_200M.Year, big_200M_Women.resid, color = 'm')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for 200M')
+plt.legend(['200M Men Residuals', '200M Women Residuals'])
+
+plt.subplot(323)
+plt.scatter(Mens_1500M.Year, big_1500M_Men.resid, color = 'c')
+plt.scatter(Womens_1500M.Year, big_1500M_Women.resid, color = 'y')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for 1500M')
+plt.legend(['1500M Men Residuals', '1500M Women Residuals'])
+
+plt.subplot(324)
+plt.scatter(Mens_SP.Year, big_Mens_SP.resid, color = 'b')
+plt.scatter(Womens_SP.Year, big_Womens_SP.resid, color = 'g')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for Shot Put')
+plt.legend(['Shot Put Men Residuals', 'Shot Put Women Residuals'])
+
+plt.subplot(325)
+plt.scatter(Mens_LJ.Year, big_Mens_LJ.resid, color = 'b')
+plt.scatter(Womens_LJ.Year, big_Womens_LJ.resid, color = 'g')
+plt.xlabel('Year')
+plt.ylabel('Residual')
+plt.title('Residuals for Long Jump')
+plt.legend(['Long Jump Men Residuals', 'Long Jump Women Residuals'])
+
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.25)
+plt.savefig('unstd_mlr_residual_results.png')
+plt.show()
+
+
+# In[47]:
+
+
+#Unstandardized Normal Probability Plots for Multiple Linear Regression Models
+plt.figure(figsize=(20, 10))
+plt.subplot(251)
+#100M Men
+res_prob_100m_m = stats.probplot(big_100M_Men.resid, plot= plt)
+plt.title('100M Men Probability Plot')
+
+plt.subplot(252)
+#100M Women
+res_prob_100m_w = stats.probplot(big_100M_Women.resid, plot= plt)
+plt.title('100M Women Probability Plot')
+
+plt.subplot(253)
+#200M Men
+res_prob_200m_m = stats.probplot(big_200M_Men.resid, plot= plt)
+plt.title('200M Men Probability Plot')
+
+plt.subplot(254)
+#200M Women
+res_prob_200m_w = stats.probplot(big_200M_Women.resid, plot= plt)
+plt.title('200M Women Probability Plot')
+
+plt.subplot(255)
+#1500M Men
+res_prob_1500m_m = stats.probplot(big_1500M_Men.resid, plot= plt)
+plt.title('1500M Men Probability Plot')
+
+plt.subplot(256)
+#1500M Women
+res_prob_1500m_w = stats.probplot(big_1500M_Women.resid, plot= plt)
+plt.title('1500M Women Probability Plot')
+
+plt.subplot(257)
+#Men's Shot Put
+res_prob_sp_m = stats.probplot(big_Mens_SP.resid, plot= plt)
+plt.title('Shot Put Men Probability Plot')
+
+plt.subplot(258)
+#Women's Shot Put
+res_prob_sp_w = stats.probplot(big_Womens_SP.resid, plot= plt)
+plt.title('Shot Put Women Probability Plot')
+
+plt.subplot(259)
+#Men's Long Jump
+res_prob_lj_m = stats.probplot(big_Mens_LJ.resid, plot= plt)
+plt.title('Long Jump Men Probability Plot')
+
+plt.subplot(2,5,10)
+#Women's Long Jump
+res_prob_lj_w = stats.probplot(big_Womens_LJ.resid, plot= plt)
+plt.title('Long Jump Women Probability Plot')
+
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.25, hspace=0.25)
+plt.savefig('unstd_mlr_normprob_results.png')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
