fd8a02544faf455751ebeff8f8213ef9a5f703e5
Parker Joncus <parkerj@me.com>
2018-11-30 13:43:34 -0600

    added correlation plots and single order
    Added correction plots for the numerical data. Created models for first
    order with the numerical and factor variables. Compare both adj r^2

--- script2.R
+++ script2.R
@@ -2,6 +2,7 @@ library(readr)
 library(MASS)
 library(BBmisc)
 library(leaps)
+library(corrplot)
 
 #Read data
 results <- read_csv("~/Documents/Olympic-Results-Regression/olympic-track-field-results/TF_results_phys_char.csv")
@@ -26,6 +27,35 @@ for (event in focus){
 #Create a list of the individual event data frames
 WholeData<-list(`data_100M Men`, `data_100M Women`, `data_200M Men`, `data_200M Women`, `data_1500M Men`, `data_1500M Women`, `data_Long Jump Men`, `data_Long Jump Women`, `data_Shot Put Men`, `data_Shot Put Women`)
 
+#Correlation plots
+i<-1
+for (dataset in WholeData){
+  cor(dataset[,c(4,9,10,11,12)])
+  corrplot.mixed(cor(dataset[,c(4,9,10,11,12)]), main = paste('Correlation Plot',focus[i]))
+  i<-i+1
+}
+
+#step regression with single order
+i<-1
+for (dataset in WholeData){
+  name<- paste("model",i, sep="")
+  name2<-paste("model_full",i, sep="")
+  name3<-paste("model_final",i, sep="")
+  model<-lm(seconds~1, data=dataset)
+  model_full<-lm(seconds~Year+Medal+Nationality+Age+Weight+Height, data=dataset)
+  model_final<-step(model,scope = list(lower=formula(model),upper=formula(model_full)), direction = 'both')
+  assign(name,model)
+  assign(name2,model_full)
+  assign(name3,model_final)
+  i=i+1
+}
+
+modelList1<-list(model_final1,model_final2,model_final3,model_final4,model_final5,model_final6,model_final7,model_final8,model_final9,model_final10)
+
+for (model in modelList1){
+  print(summary(model))
+}
+
 #Test for higher order and interaction regression up to squared
 #use step regression for each event starting with an empty model and ending with a full model of Y=b0+b1X+b2X^2+b3X^3.
 i<-1
@@ -42,15 +72,18 @@ for (dataset in WholeData){
   i=i+1
 }
 
-modelList<-list(model_final1,model_final2,model_final3,model_final4,model_final5,model_final6,model_final7,model_final8,model_final9,model_final10)
+modelList2<-list(model_final1,model_final2,model_final3,model_final4,model_final5,model_final6,model_final7,model_final8,model_final9,model_final10)
 
-for (model in modelList){
+for (model in modelList2){
   print(summary(model))
 }
 
 #List adj R^2 values for each event
-for (i in 1:length(modelList1)){
+for (i in 1:length(modelList2)){
   print(focus[i])
-  print(summary(modelList[[i]])[[9]])
+  print('single order')
+  print(summary(modelList1[[i]])[[9]])
+  print('higher order')
+  print(summary(modelList2[[i]])[[9]])
   print('##########################################################')
 }
