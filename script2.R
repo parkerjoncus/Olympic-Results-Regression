848f65e3448cff8a6d7885cf087d468a77f1e1dc
Parker Joncus <parkerj@me.com>
2018-11-30 13:33:32 -0600

    added script 2 to do analysis with more variables
    This script uses a similar dataset that has more variables to try to
    create a model. This script also uses higher order to see the
    interactions between the variables

---
+++ script2.R
@@ -0,0 +1,56 @@
+library(readr)
+library(MASS)
+library(BBmisc)
+library(leaps)
+
+#Read data
+results <- read_csv("~/Documents/Olympic-Results-Regression/olympic-track-field-results/TF_results_phys_char.csv")
+
+#Change categorical varibales to factors
+results$Gender<-as.factor(results$Gender)
+results$Event<-as.factor(results$Event)
+results$Location<-as.factor(results$Location)
+results$Medal<-as.factor(results$Medal)
+results$Nationality<-as.factor(results$Nationality)
+results[is.na(results$seconds),'seconds']<-as.numeric(results[is.na(results$seconds),'Result'][[1]])
+
+#Create list of the event names we are working with
+focus<-c('100M Men', '100M Women', '200M Men', '200M Women', '1500M Men', '1500M Women', 'Long Jump Men', 'Long Jump Women', 'Shot Put Men', 'Shot Put Women' )
+
+#Create new data frames for each event seperately 
+for (event in focus){
+  name<- paste("data_",event, sep="")
+  assign(name,results[results$Event==event,])
+}
+
+#Create a list of the individual event data frames
+WholeData<-list(`data_100M Men`, `data_100M Women`, `data_200M Men`, `data_200M Women`, `data_1500M Men`, `data_1500M Women`, `data_Long Jump Men`, `data_Long Jump Women`, `data_Shot Put Men`, `data_Shot Put Women`)
+
+#Test for higher order and interaction regression up to squared
+#use step regression for each event starting with an empty model and ending with a full model of Y=b0+b1X+b2X^2+b3X^3.
+i<-1
+for (dataset in WholeData){
+  name<- paste("model",i, sep="")
+  name2<-paste("model_full",i, sep="")
+  name3<-paste("model_final",i, sep="")
+  model<-lm(seconds~1, data=dataset)
+  model_full<-lm(seconds~Year+Medal+Nationality+Age+Weight+Height+I(Year*Age)+I(Year*Weight)+I(Year*Height)+I(Age*Weight)+I(Age*Height)+I(Height*Weight)+I(Year^2)+I(Age^2)+I(Weight^2)+I(Height^2), data=dataset)
+  model_final<-step(model,scope = list(lower=formula(model),upper=formula(model_full)), direction = 'both')
+  assign(name,model)
+  assign(name2,model_full)
+  assign(name3,model_final)
+  i=i+1
+}
+
+modelList<-list(model_final1,model_final2,model_final3,model_final4,model_final5,model_final6,model_final7,model_final8,model_final9,model_final10)
+
+for (model in modelList){
+  print(summary(model))
+}
+
+#List adj R^2 values for each event
+for (i in 1:length(modelList1)){
+  print(focus[i])
+  print(summary(modelList[[i]])[[9]])
+  print('##########################################################')
+}
